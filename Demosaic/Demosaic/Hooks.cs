90a0602ea74580fa111e87774c391d2c12467b7f
MeidoDev <MeidoDev@users.noreply.github.com>
2018-09-29 17:41:04 +0200

    Updated Demosaic for retail version

---
+++ Demosaic/Demosaic/Hooks.cs
@@ -0,0 +1,72 @@
+﻿using BepInEx.Logging;
+using Logger = BepInEx.Logger;
+using Harmony;
+using UnityEngine;
+
+namespace Demosaic
+{
+    public static class Hooks
+    {
+        public static void InstallHooks()
+        {
+            try
+            {
+                var harmony = HarmonyInstance.Create("meidodev.io.demosaic");
+                harmony.PatchAll(typeof(Hooks));
+            }
+            catch (System.Exception e)
+            {
+                Logger.Log(LogLevel.Error, e.Message);
+            }
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(TitleScript), "Awake")]
+        public static void TitleScript_Awake_Post() =>
+            // Disable anal mosaic rendering
+            ConfigClass.AnalMoza = false;
+
+        [HarmonyPostfix, HarmonyPatch(typeof(CostumeSetUp_CH01), "CharacterSetUp")]
+        public static void CostumeSetup_CharacterSetUp_Post(CostumeSetUp_CH01 __instance)
+        {
+            for (int i = 0; i < __instance.MeshObj.Count; i++)
+                if (__instance.MeshObj[i].name.Contains("moza"))
+                    __instance.MeshObj[i].GetComponent<Renderer>().enabled = false;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(CostumeSetUp_CH02), "CharacterSetUp")]
+        public static void CostumeSetup_CharacterSetUp_Post(CostumeSetUp_CH02 __instance)
+        {
+            for (int i = 0; i < __instance.MeshObj.Count; i++)
+                if (__instance.MeshObj[i].name.Contains("moza"))
+                    __instance.MeshObj[i].GetComponent<Renderer>().enabled = false;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(CostumeSetUp_PC), "CharacterSetUp")]
+        public static void CostumeSetup_CharacterSetUp_Post(CostumeSetUp_PC __instance)
+        {
+            for (int i = 0; i < __instance.MeshObj.Count; i++)
+                if (__instance.MeshObj[i].name.Contains("moza"))
+                    __instance.MeshObj[i].GetComponent<Renderer>().enabled = false;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(CostumeSetUp_SY), "CharacterSetUp")]
+        public static void CostumeSetup_CharacterSetUp_Post(CostumeSetUp_SY __instance)
+        {
+            for (int i = 0; i < __instance.MeshObj.Count; i++)
+                if (__instance.MeshObj[i].name.Contains("moza"))
+                    __instance.MeshObj[i].GetComponent<Renderer>().enabled = false;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(DanmenPixel), "Start")]
+        public static void DanmenPixel_Start_Post()
+        {
+            Renderer r1 = GameObject.Find("PC00/PC0000/PC00_ute05_moza").gameObject.GetComponent<Renderer>();
+            if (r1)
+                r1.enabled = false;
+
+            Renderer r2 = GameObject.Find("PC00/PC0000/PC00_ute05_moza_ANA").gameObject.GetComponent<Renderer>();
+            if (r2)
+                r2.enabled = false;
+        }
+    }
+}
--- Demosaic/Demosaic/IODemosaic.cs
+++ Demosaic/Demosaic/IODemosaic.cs
@@ -4,40 +4,12 @@ using UnityEngine;
 
 namespace Demosaic
 {
-    [BepInPlugin(GUID: "meidodev.io.demosaic", Name: "IO Demosaic (trial)", Version: "0.2")]
+    [BepInPlugin(GUID: "meidodev.io.demosaic", Name: "IO Demosaic", Version: "1.0")]
     public class IODemosaic : BaseUnityPlugin
     {
-        void OnLevelWasLoaded()
+        public void Awake()
         {
-            // Remove references to female mosaic meshes
-            CostumeSetUp costumeSetUp = GameObject.FindObjectOfType<CostumeSetUp>();
-            foreach (GameObject gameObj in costumeSetUp.MeshObj)
-            {
-                if (gameObj.name.Contains("moza"))
-                {
-                    costumeSetUp.MeshObj.Remove(gameObj);
-                }
-            }
-        }
-
-        void Update()
-        {
-            // Disable male mosaic
-            MozaicSetUp mozaicSetUp = GameObject.FindObjectOfType<MozaicSetUp>();
-            mozaicSetUp.MozaObj.enabled = false;
-
-            // Disable danmen (xray) mosaic
-            Renderer PC00_ute05_moza = GameObject.Find("PC00/PC0000/PC00_ute05_moza").gameObject.GetComponent<Renderer>();
-            if (PC00_ute05_moza)
-            {
-                PC00_ute05_moza.enabled = false;
-            }
-
-            Renderer PC00_ute05_moza_ANA = GameObject.Find("PC00/PC0000/PC00_ute05_moza_ANA").gameObject.GetComponent<Renderer>();
-            if (PC00_ute05_moza_ANA)
-            {
-                PC00_ute05_moza_ANA.enabled = false;
-            }
+            Hooks.InstallHooks();
         }
     }
 }
